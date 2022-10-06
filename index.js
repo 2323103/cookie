a655aaab4307989055645fce15ba370d2cb9d1fe
2323103 <115160988+2323103@users.noreply.github.com>
2022-10-06 09:00:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//for testing
+function inf(){return Infinity * Infinity}
+function GetCookies(amt){
+    Game.Earn(amt);
+}
+function power(){Game.OpenSesame()};
+function ach(){Game.SetAllAchievs(1);}
+console.log('Finished.');
+return 'test'
